da8625c956ac8d6bf68d56e523fa816fa93a2a7b
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-02-19 21:40:53 +0530

    careate othe folder and complete datatpe_02

--- basic/dataTypes_02.py
+++ basic/dataTypes_02.py
@@ -1,19 +1,55 @@
 
-# Neumaric
-     # integers
-     # Floating point numbers
-     #complex numbers
-     
-# Dictonaries(mapping)
-
-
-# Boolean
+# 1 Neumaric
+     # integers (int)
+           # whole numbers with out decimal points 
+                #ex-
+                  # 1 , 10 , 100 , 435 , 548791200 , etc...
+                  
+     # Floating point numbers (float)
+           # whole numbers with  decimal points
+              #ex-
+                  # 5.2 , 3255.325 , etc...
+                  
+     # complex numbers
+              
+              
+# 2 Dictonaries(mapping)
+         # store  key value pairs
+         # unorderd - 
+         # unique - keys shoud be unique
+         # mutable - we can add / modified / remove key value after the creation
+       #ex-
+         # my_dict = {'Alex':2,'ben':4,'bob':3,'fred':6}
+          # my_dict('Alex')  <- get values from dict
+# 3  Boolean
      # True or False
 
-# Set
-   
+# 4  Set
+    # unorderd  -  does not maintain order
+    # Uncangable  - sets cant change (immmutable)
+    # Hectrogenous - can contans all types 
+    # unique - does not contain duplicates
+   #ex-
+     # set = {12,'alex',45,50,'ben'}
 
-# Sequence type
+# 5  Sequence type
      # String
+         # ex - 
+             # name = "Amal"
      # List
+        # orded - maintan the order of the data insertion
+        # changable - mutable
+        # Heterogeneous - can contains diffferent datas
+        # contains duplicate - allow duplicates
+        
+        # ex - 
+         # list = [12,"Amal",12.3,[12,"Kamal",12],200,"stop",{12,'alex',45,50,'ben'}]
+         
      # Tuples
+        # orded - maintan the order of the data insertion
+        # Unchangable - immutable   
+        # Heterogeneous - can contains diffferent datas
+        # contains duplicate - allow duplicates
+        
+        # ex - 
+         # tuples = (12,"Amal",12.3,[12,"Kamal",12],200,"stop",{12,'alex',45,50,'ben'})
