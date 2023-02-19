0ed9518e24cd0df8c53c8c3e96cf8ee2e61d2f0b
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-02-19 20:04:15 +0530

    create env and create folder basic

---
+++ basic/dataTypes_02.py
@@ -0,0 +1,19 @@
+
+# Neumaric
+     # integers
+     # Floating point numbers
+     #complex numbers
+     
+# Dictonaries(mapping)
+
+
+# Boolean
+     # True or False
+
+# Set
+   
+
+# Sequence type
+     # String
+     # List
+     # Tuples
---
+++ basic/intro_01.py
@@ -0,0 +1,19 @@
+# Python is an interpreted, object-oriented, high-level programming language with dynamic semantics , python is dynamic type
+
+# first programm
+print("Hello, world!")
+
+# key board inputs
+input("What is your name ?")
+print("Your name is "+input("What is your name ?"))
+
+#comments
+# is use for comments
+
+#Variables
+name = "Amal"
+age = 20
+age = 2.0
+
+
+
